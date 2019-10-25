04d219f640d969e4d43b8b44977b8298f5cc917e
Dan Ware <scandal.uk@gmail.com>
2019-10-25 16:00:53 +0100

    Added experimental test prototype for the filter

---
+++ FluentValidationLister.Tests/TestActionFilter.cs
@@ -0,0 +1,48 @@
+﻿namespace FluentValidationLister.Tests
+{
+    using FluentAssertions;
+    using FluentValidationLister.Filter;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Abstractions;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+    using Microsoft.AspNetCore.Routing;
+    using Microsoft.Extensions.Primitives;
+    using Moq;
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class TestActionFilter
+    {
+        /// <summary>
+        /// This is incomplete - I have been experimenting a prototype unit test to test the filter.
+        /// </summary>
+        [Fact]
+        public void ActionFilter_ReturnsJsonResult()
+        {
+            var modelState = new ModelStateDictionary();
+
+            var httpContextMock = new DefaultHttpContext();
+            httpContextMock.Request.Query = new QueryCollection(new Dictionary<string, StringValues> { { "validate", new StringValues("1") } });
+
+            var actionContext = new ActionContext(
+                httpContextMock,
+                Mock.Of<RouteData>(),
+                Mock.Of<ActionDescriptor>(),
+                modelState
+            );
+
+            var actionExecutingContext = new ActionExecutingContext(
+                actionContext,
+                new List<IFilterMetadata>(),
+                new Dictionary<string, object>(),
+                Mock.Of<Controller>());
+
+            var validationFilter = new ValidationActionFilter();
+            validationFilter.OnActionExecuting(actionExecutingContext);
+
+            actionExecutingContext.Result.Should().BeOfType<EmptyResult>();
+        }
+    }
+}
