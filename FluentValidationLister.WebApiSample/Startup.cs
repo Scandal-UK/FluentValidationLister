9aecc0311791c49cfff69fe32b02268556573b9c
Dan Ware <scandal.uk@gmail.com>
2019-10-31 17:44:14 +0000

    Added a very basic example to demo the listing

--- FluentValidationLister.WebApiSample/Startup.cs
+++ FluentValidationLister.WebApiSample/Startup.cs
@@ -34,6 +34,8 @@ namespace FluentValidationLister.WebApiSample
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
