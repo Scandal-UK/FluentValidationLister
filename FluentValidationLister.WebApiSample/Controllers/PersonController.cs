391f3295dcc69e1951a0a5e7d4ca1fedad4ebcff
Dan Ware <scandal.uk@gmail.com>
2019-11-04 14:47:57 +0000

    Added simple endpoint to return a populated model

--- FluentValidationLister.WebApiSample/Controllers/PersonController.cs
+++ FluentValidationLister.WebApiSample/Controllers/PersonController.cs
@@ -25,5 +25,42 @@
 
             return this.Ok(new { Message = $"Person ({person.Forename} {person.Surname}) has been validated successfully!" });
         }
+
+        /// <summary>Gets a person based on the supplied ID.</summary>
+        /// <param name="id">Primary key of the <see cref="Person"/>.</param>
+        /// <response code="200">Found and returned a <see cref="Person"/>.</response>
+        /// <response code="404">The supplied ID was not found.</response>
+        /// <returns>Instance of <see cref="Person"/>.</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(OkResult), 200)]
+        [ProducesResponseType(typeof(NotFoundResult), 404)]
+        public IActionResult GetPerson(int id)
+        {
+            if (id < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
+
+            if (id > 3)
+            {
+                return NotFound();
+            }
+
+            return Ok(new Person
+            {
+                Id = id,
+                Surname = "Sample",
+                Forename = "Bob",
+                Email = "bob.sample@email.com",
+                Address = new Address
+                {
+                    Line1 = "123 Sample Street",
+                    Town = "Sample Town",
+                    County = "Sampleshire",
+                    Postcode = "SA1 4PL",
+                    Country = new Country { Name = "England" },
+                },
+            });
+        }
     }
 }
