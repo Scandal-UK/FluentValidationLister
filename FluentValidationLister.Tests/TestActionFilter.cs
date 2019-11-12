51e01eff8be20c401f4539a161e1a0ffabb6d2e7
Dan Ware <scandal.uk@gmail.com>
2019-11-12 23:28:47 +0000

    Removed some unused dependencies

--- FluentValidationLister.Tests/TestActionFilter.cs
+++ FluentValidationLister.Tests/TestActionFilter.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using FluentValidationLister.Filter;
     using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Http.Internal;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Abstractions;
     using Microsoft.AspNetCore.Mvc.Filters;
