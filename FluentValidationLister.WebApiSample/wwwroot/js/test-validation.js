c3b980e133cdc06ba5d753ca5a36fe3c8635b6a5
Dan Ware <scandal.uk@gmail.com>
2019-11-15 14:00:07 +0000

    Added checkbox to toggle clientside validation

--- FluentValidationLister.WebApiSample/wwwroot/js/test-validation.js
+++ FluentValidationLister.WebApiSample/wwwroot/js/test-validation.js
@@ -1,36 +1,37 @@
 ﻿(function (window, document, $) {
-    // Button, Form, Div
-    var populateForm, personForm, resultPanel;
+    // Checkbox, Button, Form, Div
+    var performClientsideValidation, populateForm, personForm, resultPanel;
 
-    var ruleList, messageList;
+    // Response from FluentValidationLister
+    var validationList;
 
+    // todo: rewrite script for jQuery
     var validateForm = function () {
         var data = getFormValues();
         var form = $(personForm);
         var errorList = [];
 
-        console.log(data);
-        for (var fieldName in ruleList)
-        {
-            if (fieldName !== "address") {
-                var valid = true;
-                for (var ruleName in ruleList[fieldName]) {
-                    switch (ruleName) {
-                        case "required":
-                            if (!data[fieldName] || data[fieldName] === "") {
-                                valid = false;
-                                errorList.push(messageList[fieldName][ruleName]);
-                            }
-                            break;
-                    }
+        $.each(data, function (fieldName, fieldValue) {
+            var valid = true;
+            for (var ruleName in validationList.validatorList[fieldName]) {
+                switch (ruleName) {
+                    case "required":
+                        if (!fieldValue || fieldValue === "") {
+                            valid = false;
+                            errorList.push(validationList.errorList[fieldName][ruleName]);
+                        }
+                        break;
+                    // todo: default to AJAX validation for any unrecognised ruleName
                 }
-
-                if (valid === false) $('[name="' + fieldName + '"]', form).addClass("error");
             }
-        }
+
+            if (valid === false) $('[name="' + fieldName + '"]', form).addClass("error");
+        });
 
         if (errorList.length > 0)
         {
+            if (console.error) console.error("Clientside validation failed!");
+
             var list = $("<ul />").addClass("error");
             $.each(errorList, function (i, val) {
                 list.append($("<li />").text(val));
@@ -48,17 +49,18 @@
         if (personForm.disabled === true) return;
         resetResult();
 
-        if (validateForm() === true)
-        {
-            personForm.disabled = true;
-
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = displayFormResult;
-            xhr.open("POST", "api/Person", true);
-            xhr.setRequestHeader("Content-Type", "application/json");
-
-            xhr.send(JSON.stringify(getFormValues(true)));
+        if ($(performClientsideValidation).is(":checked")) {
+            if (validateForm() === false) return;
         }
+
+        personForm.disabled = true;
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = displayFormResult;
+        xhr.open("POST", "api/Person", true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.send(JSON.stringify(getFormValues(true)));
     };
 
     var displayFormResult = function () {
@@ -67,6 +69,8 @@
 
             var validationResponse = JSON.parse(this.responseText);
             if (this.status === 400) {
+                if (console.error) console.error("Server-side validation failed!");
+
                 var form = $(personForm);
                 var errorList = [];
 
@@ -95,50 +99,47 @@
     var getFormValues = function (splitField) {
         var data = {};
         $(personForm).serializeArray().map(function (field) {
-            if (field.value !== "") {
-                if (splitField === true && field.name.includes(".")) {
-                    var split = field.name.split(".");
-                    if (!data[split[0]]) data[split[0]] = {};
+            if (splitField === true && field.name.includes(".")) {
+                var split = field.name.split(".");
+                if (!data[split[0]]) data[split[0]] = {};
 
-                    data[split[0]][split[1]] = field.value;
+                data[split[0]][split[1]] = field.value;
 
-                } else {
-                    data[field.name] = field.value;
-                }
+            } else {
+                data[field.name] = field.value;
             }
         });
 
         // Correct any string-types that should be numeric or Boolean (only one in this example)
-        if (data.age === "") data.age = null; else if (data.age) data.age = parseInt(data.age);
+        if (data.age === "") data.age = null; else data.age = parseInt(data.age);
 
         return data;
     };
 
-    var populateFormFromServerRequest = function () {
+    var populateFormFromServer = function () {
         resetResult();
 
         var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = populateFormFromServerResponse;
+        xhr.onreadystatechange = function () {
+            if (this.readyState === 4) {
+                var form = $(personForm);
+                var json = JSON.parse(this.responseText);
+
+                $.each(json, function (key, val) {
+                    if (key === "address") {
+                        $.each(val, function (addrKey, addrVal) {
+                            $('[name="address.' + addrKey + '"]', form).val(addrVal);
+                        });
+                    } else {
+                        $('[name="' + key + '"]', form).val(val);
+                    }
+                });
+            }
+        };
+
         xhr.open("GET", "api/Person/1", true);
         xhr.setRequestHeader("Accepts", "application/json");
         xhr.send();
-    };
-
-    var populateFormFromServerResponse = function () {
-        if (this.readyState === 4) {
-            var form = $(personForm);
-            var json = JSON.parse(this.responseText);
-
-            $.each(json, function (key, val) {
-                if (key === "address") {
-                    $.each(val, function (addrKey, addrVal) {
-                        $('[name="address.' + addrKey + '"]', form).val(addrVal);
-                    });
-                } else {
-                    $('[name="' + key + '"]', form).val(val);
-                }
-            });
-        }
     };
 
     var resetResult = function () {
@@ -148,28 +149,24 @@
 
     var getValidationRules = function () {
         var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = setValidationRules;
+        xhr.onreadystatechange = function () {
+            if (this.readyState === 4) validationList = JSON.parse(this.responseText);
+        };
+
         xhr.open("POST", "api/Person?validation=true", true);
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.send(JSON.stringify({ person: 1 }));
     };
 
-    var setValidationRules = function () {
-        if (this.readyState === 4) {
-            var validationResponse = JSON.parse(this.responseText);
-            ruleList = validationResponse.validatorList;
-            messageList = validationResponse.errorList;
-        }
-    };
-
     window.addEventListener("load", function () {
         getValidationRules();
 
-        populateForm = document.getElementById("populate-form"); // Button
-        personForm = document.getElementById("person-form"); // Form
-        resultPanel = document.getElementById("result-panel"); // Div
+        performClientsideValidation = document.getElementById("performClientsideValidation"); // Checkbox
+        populateForm = document.getElementById("populateFormButton"); // Button
+        personForm = document.getElementById("personForm"); // Form
+        resultPanel = document.getElementById("resultPanel"); // Div
 
-        populateForm.addEventListener("click", populateFormFromServerRequest);
+        populateForm.addEventListener("click", populateFormFromServer);
         personForm.addEventListener("submit", submitForm);
         personForm.addEventListener("reset", resetResult);
     });
