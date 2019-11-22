06174559b42c4d0bfee3578b2bd480306e60e729
Dan Ware <scandal.uk@gmail.com>
2019-11-22 16:56:03 +0000

    Formatting gulp file

--- FluentValidationLister.WebApiSample/gulpfile.js
+++ FluentValidationLister.WebApiSample/gulpfile.js
@@ -1,8 +1,4 @@
 ﻿/// <binding AfterBuild='default' Clean='clean' />
-/*
-This file is the main entry point for defining Gulp tasks and using Gulp plugins.
-Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
-*/
 
 var gulp = require('gulp');
 var del = require('del');
@@ -16,5 +12,7 @@ gulp.task('clean', function () {
 });
 
 gulp.task('default', function () {
-    return gulp.src(paths.scripts).pipe(gulp.dest('wwwroot/js'));
+    return gulp
+        .src(paths.scripts)
+        .pipe(gulp.dest('wwwroot/js'));
 });
