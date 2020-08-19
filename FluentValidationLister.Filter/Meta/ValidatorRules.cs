1ae7949c5498f1981c93b654cd020060b693914b
Dan Ware <scandal.uk@gmail.com>
2020-08-19 20:31:24 +0100

    Added new property for types

--- FluentValidationLister.Filter/Meta/ValidatorRules.cs
+++ FluentValidationLister.Filter/Meta/ValidatorRules.cs
@@ -12,5 +12,8 @@
 
         /// <summary>Gets or sets a list of error messages indexed by property name.</summary>
         public SerialisableDictionary<string, SerialisableDictionary<string, string>> ErrorList { get; set; } = new SerialisableDictionary<string, SerialisableDictionary<string, string>>();
+
+        /// <summary>Gets or sets a list of types indexed by property name.</summary>
+        public SerialisableDictionary<string, string> TypeList { get; set; } = new SerialisableDictionary<string, string>();
     }
 }
