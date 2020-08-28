928a6f0184be01d625f1789173f054f24c56563d
Dan Ware <scandal.uk@gmail.com>
2020-08-28 18:57:12 +0100

    Plit type-checking unit tests into their own class

--- FluentValidationLister.Tests/TestListingRules.cs
+++ FluentValidationLister.Tests/TestListingRules.cs
@@ -98,30 +98,6 @@
             TestListingHelper.GetValidatorRules(v => v.RuleForEach(x => x.Orders).SetValidator(new OrderValidator()))
                 .ValidatorList["Orders.Amount"].Should().NotBeNull();
 
-        [Fact(DisplayName = "Types include string")]
-        public void Types_IncludeString() =>
-            TestListingHelper.GetValidatorRules().TypeList["Surname"].Should().Be("string");
-
-        [Fact(DisplayName = "Types include number")]
-        public void Types_IncludeNumber() =>
-            TestListingHelper.GetValidatorRules().TypeList["Id"].Should().Be("number");
-
-        [Fact(DisplayName = "Types include boolean")]
-        public void Types_IncludeBoolean() =>
-            TestListingHelper.GetValidatorRules().TypeList["IsActive"].Should().Be("boolean");
-
-        [Fact(DisplayName = "Types include date")]
-        public void Types_IncludeDate() =>
-            TestListingHelper.GetValidatorRules().TypeList["DateOfBirth"].Should().Be("date");
-
-        [Fact(DisplayName = "Types include child types")]
-        public void Types_IncludeChildTypes() =>
-            TestListingHelper.GetValidatorRules().TypeList["Address.Line1"].Should().Be("string");
-
-        [Fact(DisplayName = "Types include collection types")]
-        public void Types_IncludeCollectionTypes() =>
-            TestListingHelper.GetValidatorRules().TypeList["Orders.Amount"].Should().Be("number");
-
         [Fact(DisplayName = "When serialised returns correct message")]
         public void WhenSerialised_ReturnsCorrectMessage() =>
             TestListingHelper.GetDeserialisedValidatorRules(TestListingHelper.GetValidatorRules(v => v.RuleFor(x => x.Forename).NotEmpty()))["errorList"]["forename"]["required"].ToString().Should().Be("'Forename' must not be empty.");
---
+++ FluentValidationLister.Tests/TestListingTypes.cs
@@ -0,0 +1,32 @@
+﻿namespace FluentValidationLister.Tests
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class TestListingTypes
+    {
+        [Fact(DisplayName = "Types include string")]
+        public void Types_IncludeString() =>
+            TestListingHelper.GetValidatorRules().TypeList["Surname"].Should().Be("string");
+
+        [Fact(DisplayName = "Types include number")]
+        public void Types_IncludeNumber() =>
+            TestListingHelper.GetValidatorRules().TypeList["Id"].Should().Be("number");
+
+        [Fact(DisplayName = "Types include boolean")]
+        public void Types_IncludeBoolean() =>
+            TestListingHelper.GetValidatorRules().TypeList["IsActive"].Should().Be("boolean");
+
+        [Fact(DisplayName = "Types include date")]
+        public void Types_IncludeDate() =>
+            TestListingHelper.GetValidatorRules().TypeList["DateOfBirth"].Should().Be("date");
+
+        [Fact(DisplayName = "Types include child types")]
+        public void Types_IncludeChildTypes() =>
+            TestListingHelper.GetValidatorRules().TypeList["Address.Line1"].Should().Be("string");
+
+        [Fact(DisplayName = "Types include collection types")]
+        public void Types_IncludeCollectionTypes() =>
+            TestListingHelper.GetValidatorRules().TypeList["Orders.Amount"].Should().Be("number");
+    }
+}
